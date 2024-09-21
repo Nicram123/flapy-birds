81e84d09bde799a15d404c0571bd062340edfca9
Nicram123 <marcin2003s@gmail.com>
2024-09-22 01:27:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import sys 
+from constants import screen_height, screen_width
+
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pic = pygame.image.load('background.png')
+pic = pygame.transform.scale(pic, (screen_width, screen_height))
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+  screen.blit(pic, (0, 0))
+  pygame.display.update()
+
