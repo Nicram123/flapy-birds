c847b7007339ff9bc00c60776f4535c882972c60
Nicram123 <marcin2003s@gmail.com>
2024-09-22 01:29:34 +0200

    my second commit

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pic = pygame.image.load('background.png')
 pic = pygame.transform.scale(pic, (screen_width, screen_height))
 
+
+
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
